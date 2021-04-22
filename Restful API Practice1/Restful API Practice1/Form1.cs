f3335270520bb48b8aabcf74f22221c72b8db4d3
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-22 09:00:39 -0400

    Add files via upload

---
+++ Restful API Practice1/Restful API Practice1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restful_API_Practice1
+{
+    public partial class RestfulApp : Form
+    {
+        public RestfulApp()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Event Handler
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RestClient rClient = new RestClient();
+            rClient.endPoint = txtRequestURI.Text;
+
+            debugOutput("Rest Client Created");
+
+            string strResponse = string.Empty;
+
+            strResponse = rClient.makeRequest();
+
+            debugOutput(strResponse);
+
+        }
+
+        #endregion
+
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }
+
+    }
+}
